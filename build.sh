6784e18c8bb29824568ae8a49bf0a4a99b8eb3a7
CodeVirggin <78057753+ambbrose@users.noreply.github.com>
2023-02-19 17:46:49 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 echo "Build the project - install the packages"
 python3.9 -m pip install -r requirements.txt
-
 echo "Making Migrations"
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
-
 echo "Collecting Static Files"
 python3.9 manage.py collectstatic --noinput --clear
