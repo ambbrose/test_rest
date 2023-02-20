a0c6d185d483e20370f927b3e87a350451a6c61e
CodeVirggin <78057753+ambbrose@users.noreply.github.com>
2023-02-20 13:49:51 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
 # build_files.sh
+echo "starting to upgrade pip"
 pip install --upgrade pip
+echo "using pip to unstall from requirements.txt baby"
 pip install -r requirements.txt
+echo "stating to arrange static files"
 python manage.py collectstatic --noinput
