4fbe016c54f9b8d68ebef2efafedbc031b64af67
CodeVirggin <78057753+ambbrose@users.noreply.github.com>
2023-02-20 14:54:42 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,17 @@
 # build_files.sh
 echo "starting to upgrade pip"
 pip install --upgrade pip
-echo "using pip to unstall from requirements.txt baby"
-pip install -r requirements.txt
+echo "using pip to install the packages"
+pip install asgiref==3.6.0
+pip install backports.zoneinfo==0.2.1
+pip install dj-database-url==1.2.0
+pip install Django==4.1.3
+pip install djangorestframework==3.14.0
+pip install mysqlclient==2.1.1
+pip install Pillow==9.4.0
+pip install python-decouple==3.7
+pip install pytz==2022.7.1
+pip install sqlparse==0.4.3
+pip install tzdata==2022.7
 echo "stating to arrange static files"
 python manage.py collectstatic --noinput
